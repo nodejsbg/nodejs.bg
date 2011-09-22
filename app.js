c68f8a39c2553e32a79856910290c4406a3ccc86
Veselin Todorov <veselin@veselin.bg>
2011-09-22 16:35:43 +0300

    Removing the less support

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
